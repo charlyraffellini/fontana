cc116e893eacdb313938f0add408bf7d5d6f82bb
charlyraffellini <del0al99@gmail.com>
2013-05-29 21:24:45 -0700

    Agrego comentarios de como debe trabajar la configuracion con los componentes que devuelve el Reader.

--- src/utn/tadp/fontana/configuracion/Configuracion.java
+++ src/utn/tadp/fontana/configuracion/Configuracion.java
@@ -5,6 +5,21 @@ import java.util.Map;
 
 import utn.tadp.fontana.Dependencia;
 
+/**
+ * Donfiguración va a tener una lista de Componentes a los cuales les corresponde un BeanName
+ * Y además una lista de interesados para cada Componente que todavía no se agregó a la lista oficial
+ * Entonces cuando se agrega un nuevo Componente a la lista oficial se revisa si no hay interesados
+ * que lo necesiten, si es así se les manda el componente
+ * Si al final de la construccion quedan interesados por algun componente significa que hay algun error
+ * y se puede avisar de esta anomalia al usuario
+ * 
+ * Lista de componentes encontrados por el reader: <String BeanName, Componente>
+ * Listas de componentes interesados en un componente no creado: <String FutureBeanName, ListaDeInteresadosASettearle<Componentes>>
+ * 
+ * 
+ * @author charly
+ *
+ */
 public class Configuracion {
 
 	private Reader reader;
