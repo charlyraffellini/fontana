1ef6e76914d8d2cf0c5bf3351433d677be4c8cde
charlyraffellini <del0al99@gmail.com>
2013-05-30 23:11:24 -0700

    Agrego las clases necesarias para Configuración y un test.

--- src/utn/tadp/fontana/configuracion/Configuracion.java
+++ src/utn/tadp/fontana/configuracion/Configuracion.java
@@ -1,6 +1,8 @@
 package utn.tadp.fontana.configuracion;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import utn.tadp.fontana.Dependencia;
@@ -22,9 +24,37 @@ import utn.tadp.fontana.Dependencia;
  */
 public class Configuracion {
 
-	private Reader reader;
 	private Map<String,Bean> beans = new HashMap<String,Bean>();
+	private Map<String,List<Solicitud>> listasDeSolicitud = new HashMap<String,List<Solicitud>>();
 	
+	public void setBeanLike(String beanName, Bean interesado, String property){
+		Bean pedido = this.beans.get(beanName);
+		if(pedido == null) this.agregarSolicitud(beanName, new Solicitud(interesado, property));
+		else interesado.addComponente(beanName, pedido);
+	}
+	
+	public void addBean(String beanName, Bean bean){
+		this.beans.put(beanName, bean);
+		List<Solicitud> solicitudes = this.listasDeSolicitud.get(beanName);
+		if(solicitudes != null){
+			for(Solicitud solicitud : solicitudes){
+				solicitud.getBean().addComponente(solicitud.getProperty(), bean);
+			}
+		}
+	}
+	
+	private void agregarSolicitud(String beanName, Solicitud solicitud) {
+		List<Solicitud> solicitudes = this.listasDeSolicitud.get(beanName);
+		if (solicitudes == null) solicitudes = this.nuevaListaInteresados(beanName); 
+		solicitudes.add(solicitud);
+	}
+
+	private List<Solicitud> nuevaListaInteresados(String beanName) {
+		List<Solicitud> solicitudes = new ArrayList<Solicitud>();
+		this.listasDeSolicitud.put(beanName, solicitudes);
+		return solicitudes;
+	}
+
 	public Dependencia getDependencia(Bean comp){
 		//Buscar en el map y pedirle la dependencia al bean que va a tener el
 		//mismo nombre que el que paso por parametro
